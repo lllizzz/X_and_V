9ebd79ebf4555e3be50201c6f4d2a5be832dbfa3
Elizaveta <elizaveta.shetkina@gmail.com>
2023-11-05 22:09:55 +0300

    X_and_V

---
+++ X_and_V.cpp
@@ -0,0 +1,59 @@
+﻿#include <list>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iterator>
+
+using namespace std;
+
+void SetText(list<string>& text)
+{
+    string words;
+    while (true)
+    {
+        getline(cin, words);
+        if (words.empty())
+        {
+            break;
+        }
+        text.push_back(words);
+    }
+}
+
+void NewText(list<string>& text)
+{
+    auto iter = text.begin();
+    string copy;
+    string commands;
+    while (cin >> commands)
+    {
+        if (commands == "Down" && iter != text.end())
+        {
+            ++iter;
+        }
+        else if (commands == "Up" && iter != text.begin())
+        {
+            --iter;
+        }
+        else if (commands == "Ctrl+X" && iter != text.end())
+        {
+            copy = *iter;
+            iter = text.erase(iter);
+        }
+        else if (commands == "Ctrl+V" && !copy.empty())
+        {
+            text.insert(iter, copy);
+        }
+    }
+    for (const auto& i : text)
+    {
+        cout << i << endl;
+    }
+}
+
+int main()
+{
+    list<string> text;
+    SetText(text);
+    NewText(text);
+}
